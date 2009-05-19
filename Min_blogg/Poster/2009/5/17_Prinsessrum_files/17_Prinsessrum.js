7360e4f18c4dd8a089a1cf6bd2125eede9900cd5
Lillyana Kihlbom <lilly@kihlbom.se>
2009-05-19 08:03:20 +0200

    Ny blog: Prinsessrum

--- Min_blogg/Poster/2009/5/17_Prinsessrum_files/17_Prinsessrum.js
+++ Min_blogg/Poster/2009/5/17_Prinsessrum_files/17_Prinsessrum.js
@@ -1,11 +1,11 @@
 // Created by iWeb 3.0.1 local-build-20090517
 
 setTransparentGifURL('../../../../Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_2:new IWShadow({blurRadius:10,offset:new IWPoint(-0.0000,2.0000),color:'#000000',opacity:0.600000}),shadow_1:new IWShadow({blurRadius:10,offset:new IWPoint(-0.0000,2.0000),color:'#000000',opacity:0.600000}),shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(-0.0000,2.0000),color:'#000000',opacity:0.600000})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_2:new IWShadow({blurRadius:10,offset:new IWPoint(-0.0000,2.0000),color:'#000000',opacity:0.600000}),shadow_0:new IWShadow({blurRadius:10,offset:new IWPoint(-0.0000,2.0000),color:'#000000',opacity:0.600000}),shadow_1:new IWShadow({blurRadius:10,offset:new IWPoint(-0.0000,2.0000),color:'#000000',opacity:0.600000})});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
-{dynamicallyPopulate();loadMozillaCSS('5_Lansering_files/5_LanseringMoz.css')
+{dynamicallyPopulate();loadMozillaCSS('17_Prinsessrum_files/17_PrinsessrumMoz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixupAllIEPNGBGs();fixAllIEPNGs('../../../../Media/transparent.gif');Widget.onload();fixupIECSS3Opacity('id4');BlogFixupPreviousNext();applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
